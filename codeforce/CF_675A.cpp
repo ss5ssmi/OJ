47e30a760d049d6cc0b4ead95cd2ede91882f214
ss5ssmi <imss5ss@outlook.com>
2017-11-14 19:24:48 +0800

    675A

---
+++ codeforce/CF_675A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(c==0){
+		if(a==b) printf("YES\n"); 
+		else printf("NO\n");
+	}else{
+		if((a>b && c>0) || (a<b && c<0)){
+			printf("NO\n");
+		}else{
+			if(abs(a-b)%c==0) printf("YES\n");
+			else printf("NO\n");
+		} 
+	}
+	return 0;
+}
