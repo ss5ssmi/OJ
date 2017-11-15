74841a579e0d2524c06c9257a57edfdbb6e2d004
ss5ssmi <imss5ss@outlook.com>
2017-11-15 15:47:56 +0800

    378A

---
+++ codeforce/CF_378A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int a,b;
+	scanf("%d%d",&a,&b);
+	int f=0,d=0,s=0;
+	for(int i=1; i<=6; i++) {
+		if(abs(i-a)>abs(i-b)) s++;
+		else if(abs(i-a)<abs(i-b)) f++;
+		else d++;
+	}
+	printf("%d %d %d\n",f,d,s);
+	return 0;
+}
