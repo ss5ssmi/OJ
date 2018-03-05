a7ef89323029173f7e9c45d5dbc02d5f99a73e8c
ss5ssmi <imss5ss@outlook.com>
2018-03-05 17:37:03 +0800

    495A

---
+++ codeforce/CF_495A.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int a,b,s[10]; 
+	s[0]=2,s[1]=7,s[2]=2,s[3]=3,s[4]=3,s[5]=4,s[6]=2,s[7]=5,s[8]=1,s[9]=2;
+	scanf("%1d%1d",&a,&b);
+	cout<<s[a]*s[b];
+	return 0;
+}
