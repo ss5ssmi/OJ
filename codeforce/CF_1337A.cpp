5811acd4cf47cd95c66096fa3d949410d01c7dbd
XingTong <intern13@east2west.cn>
2020-05-16 15:45:05 +0800

    1337A

---
+++ codeforce/CF_1337A.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int t;
+	scanf("%d", &t);
+	while(t--){
+		int a, b, c, d;
+		scanf("%d%d%d%d", &a, &b, &c, &d);
+		printf("%d %d %d\n", b, c, c);
+	}
+	return 0;
+}
